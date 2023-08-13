8b407fcdf5caf21844d545a031ecffe3c59c2a5e
farnam <bluewhale.f@gmail.com>
2023-08-13 08:39:46 +0330

    create db model for coin

--- challenge/coin/models.py
+++ challenge/coin/models.py
@@ -5,8 +5,8 @@ from utils.models import BaseModel
 class Coin(BaseModel):
     name = models.CharField(max_length=50)
     abbreviation_name = models.CharField(max_length=20)
-    purchase_price = models.DecimalField()
-    sale_price = models.DecimalField()
+    purchase_price = models.DecimalField(decimal_places=2, max_digits=18)
+    sale_price = models.DecimalField(decimal_places=2, max_digits=18)
 
     def __str__(self) -> str:
         return f"{self.name}-{self.abbreviation_name}"
