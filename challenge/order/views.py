a7f127352ec048ee5147646219dd9ef741c2e5fe
farnam <bluewhale.f@gmail.com>
2023-08-13 08:43:42 +0330

    create apis for set order sync and async

--- challenge/order/views.py
+++ challenge/order/views.py
@@ -1,44 +1,38 @@
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import status
 from rest_framework.views import APIView
 from drf_yasg.utils import swagger_auto_schema
 
 from utils.utils import validate_serializer
 from .serializer import ByCryptoSerializer, OrderSerializer
+from .helper import create_async_order, create_sync_order
 
 # Create your views here.
 
 
-import pika
-
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-channel = connection.channel()
-
-import sys, datetime
-
-
 class BuyCryptoSyncApi(APIView):
-    # permission_classes = (IsAuthenticated,)
-    # authentication_classes = [TokenAuthentication]
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = [TokenAuthentication]
 
     @swagger_auto_schema(request_body=ByCryptoSerializer)
     @validate_serializer(ByCryptoSerializer)
     def post(self, request):
         serializer = ByCryptoSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        order = serializer.create_sync(serializer.validated_data, request)
-        return Response(OrderSerializer(order).data)
+        order = create_sync_order(serializer.validated_data, request)
+        return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
 
 
 class BuyCryptoAsyncApi(APIView):
-    # permission_classes = (IsAuthenticated,)
-    # authentication_classes = [TokenAuthentication]
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = [TokenAuthentication]
 
     @swagger_auto_schema(request_body=ByCryptoSerializer)
     @validate_serializer(ByCryptoSerializer)
     def post(self, request):
         serializer = ByCryptoSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        order = serializer.create_sync(serializer.validated_data, request)
-        return Response(OrderSerializer(order).data)
+        order = create_async_order(serializer.validated_data, request)
+        return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
