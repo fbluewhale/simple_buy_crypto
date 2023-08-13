ad30c7a304d94e8a745d5dfaa3c380e30c0a6894
farnam <bluewhale.f@gmail.com>
2023-08-13 08:44:19 +0330

    update price field to decimal

--- challenge/order/models.py
+++ challenge/order/models.py
@@ -11,12 +11,18 @@ class Order(BaseModel):
         UserProfile, related_name="account", on_delete=models.PROTECT
     )
     coin = models.ForeignKey(Coin, related_name="coin", on_delete=models.PROTECT)
-    purchase_price = models.DecimalField("Crypto purchase price", default=0)
-    sale_price = models.DecimalField("Crypto sales price", default=0)
-    total_price = models.DecimalField("order total price", default=0)
+    purchase_price = models.DecimalField(
+        "Crypto purchase price", decimal_places=2, max_digits=18
+    )
+    sale_price = models.DecimalField(
+        "Crypto sales price", decimal_places=2, max_digits=18, default=0
+    )
+    total_price = models.DecimalField(
+        "order total price", decimal_places=2, max_digits=18
+    )
 
     external_checkout = models.BooleanField(default=False)
-    amount = models.DecimalField(decimal_places=1, max_digits=0)
+    amount = models.DecimalField(decimal_places=2, max_digits=18)
 
     def __str__(self):
         return f"{self.user.user.username}-{self.coin.abbreviation_name}- amount: {self.amount} - total price : {self.total_price}"
