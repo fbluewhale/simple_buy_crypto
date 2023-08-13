ba6f3d65d2e466093947960e1bf6bf0b51cbd73e
farnam <bluewhale.f@gmail.com>
2023-08-13 11:15:16 +0330

    change serializer

--- challenge/order/serializer.py
+++ challenge/order/serializer.py
@@ -11,16 +11,34 @@ from .models import Order
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = [
+            "user" "coin",
+            "purchase_price",
+            "sale_price",
+            "total_price",
+            "external_checkout",
+            "amount",
+        ]
 
 
 class ByCryptoSerializer(serializers.ModelSerializer):
     user = UserProfileSerializer(read_only=True)
-    sale_price = serializers.IntegerField(read_only=True)
-    total_price = serializers.IntegerField(read_only=True)
+    sale_price = serializers.DecimalField(
+        read_only=True, decimal_places=2, max_digits=18
+    )
+    total_price = serializers.DecimalField(
+        read_only=True, decimal_places=2, max_digits=18
+    )
     coin = CoinSerializer(read_only=True)
     coin_name = serializers.CharField()
 
     class Meta:
         model = Order
-        fields = "__all__"
+        fields = [
+            "user" "coin",
+            "purchase_price",
+            "sale_price",
+            "total_price",
+            "external_checkout",
+            "amount",
+        ]
